2a87778683cd3ddcea034b9fd00eb4efa6dd205e
9mynamemj7 <ka5560@naver.com>
2022-09-16 23:34:05 +0900

    Typescript

---
+++ TypeScript/ch12-1/src/test/getFileNameAndNumber-test.ts
@@ -0,0 +1,4 @@
+import { getFileNameAndNumber } from '../utils/getFileNameAndNumber';
+
+const [filename, numberOfFakeItems] = getFileNameAndNumber('data/fake.csv', 100000)
+console.log(filename, numberOfFakeItems)
