7d1817b2d3f84c9e8f3fc3884076c36b831189e3
9mynamemj7 <ka5560@naver.com>
2023-04-18 13:52:38 +0900

    MongoDB 학습 및 gitignore 추가

--- FE/Nodejs/boiler-plate/index.js
+++ FE/Nodejs/boiler-plate/index.js
@@ -1,3 +1,12 @@
+// mongoDB
+const myData = require('./personal.json');
+const mongoose = require('mongoose');
+
+const myHostNum = myData.mongo.host;
+mongoose.connect(myHostNum).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+// express
 const express = require('express')
 const app = express()
 const port = 3000
@@ -9,3 +18,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
