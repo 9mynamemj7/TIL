a1477179a93dc57423e4966232a08cdb7a052b86
9mynamemj7 <ka5560@naver.com>
2023-04-20 16:57:34 +0900

    Nodemon 설치와 Bcrypt를 이용한 비밀번호 암호화

--- FE/Nodejs/boiler-plate/models/User.js
+++ FE/Nodejs/boiler-plate/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRouds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -22,6 +24,20 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.pre('save', function(next){
+    var user = this;
+    if(user.isModified('password')) {
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRouds, function (err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }
+})
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
