92e463ab8bb978d00942acc06996b3cc1d591880
9mynamemj7 <ka5560@naver.com>
2023-04-21 15:20:29 +0900

    Revert "NodeJs - 로그인기능"
    This reverts commit b809eea93d00d9ea1751ee54b4c65b0b22b02fc5.

--- FE/Nodejs/boiler-plate/index.js
+++ FE/Nodejs/boiler-plate/index.js
@@ -13,13 +13,11 @@ const { User } = require('./models/User')
 app.use(express.urlencoded({extended: true}));
 // application/json 데이터를 분석해서 가져올 수 있게 만듬
 app.use(express.json());
-// 쿠키파서
-app.use(cookieParser());
+
 
 // Connect MongoDB
 const myData = require('./personal.json');
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
 
 const myHostNum = myData.mongo.host;
 mongoose.connect(myHostNum).then(() => console.log('MongoDB Connected...'))
@@ -45,33 +43,6 @@ app.post('/register', async (req, res) => {
   })
 })
 
-app.post('./login', (req, res) => {
-  // 요청된 이메일을 데이터베이스에서 있는지 찾는다
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if(!user) {
-      return res.json({
-        loginSuccess: false,
-        message: "이메일 오류"
-      })
-    }
-  
-    // 요정된 이메일이 데이터베이스에 있다면 비밀번호 확인
-    user.comparePassword(req.body.password, (err,isMatch) => {
-      if(!isMatch)
-        return res.json({ loginSuccess: false, message: "비밀번호 오류"
-      // 비밀번호도 맞다면 토큰을 생성
-      user.generateToken((err,user) => {
-        if(err) return res.status(400).send(err);
-        
-        // 토큰 저장 쿠키에
-        res.cookie("x_auth", user.token)
-        .status(200)
-        .json
-        })
-      })
-    })
-  })
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
