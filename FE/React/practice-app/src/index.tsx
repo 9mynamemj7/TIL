ca1ef102436e96bb82237049f0e3fac2f8bc87eb
9mynamemj <ka5560@naver.com>
2023-05-08 14:51:40 +0900

    App.tsx로 수정

--- FE/React/practice-app/src/index.tsx
+++ FE/React/practice-app/src/index.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from "./App";
 
-const root = document.getElementById("root");
-if (root) {
-  ReactDOM.createRoot(root).render(<App />);
-} else {
-  console.error('root element not found');
-}
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
