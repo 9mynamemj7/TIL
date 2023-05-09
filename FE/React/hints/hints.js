4bdfc1bd4158ca9bfdb340dff27df071d6a58c36
9mynamemj <ka5560@naver.com>
2023-05-09 13:15:21 +0900

    First안에 훅을 두개 다 넣음
    count 무한하게 증가중

--- FE/React/hints/hints.js
+++ FE/React/hints/hints.js
@@ -3,8 +3,23 @@
 
 // Real Time Clock
 
-const hints = {
-    useState : "상태 관리 훅",
-    useEffect : "컴포넌트 외부의 상태 관리 훅",
-    setInterval : "시간 간격마다 함수 실행!"
-}
+// const hints = {
+//     useState : "상태 관리 훅",
+//     useEffect : "컴포넌트 외부의 상태 관리 훅",
+//     setInterval : "시간 간격마다 함수 실행!"
+// }
+
+import { useState, useEffect } from 'react'
+const example = () => {
+    let initialState = 0;
+    const [state, setState] = useState(initialState);
+
+    return (
+        <div>
+            <p>{`count: ${state}`}</p>
+            <button onClick={()=>setState(state+1)}>+</button>
+        </div>
+    )
+};
+
+export default example;
