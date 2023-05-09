ab9c975a1317a28514b6b565bae7a93966931cb5
9mynamemj <ka5560@naver.com>
2023-05-09 13:15:21 +0900

    First안에 훅을 두개 다 넣음
    count 무한하게 증가중

--- FE/React/practice-app/src/pages/First.tsx
+++ FE/React/practice-app/src/pages/First.tsx
@@ -1,33 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-// const user = {
-//   firstName : 'Rose',
-//   lastName : 'Taylor'
-// };
-
-// const element = <h1>Hello, {user.firstName} {user.lastName}</h1>;
 
 const First = () => {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+    
   useEffect(() => {
-    const qustn = setInterval(()=>{
-      if (count) {
-        setCount(count + 1);
-      }
-    }, 1000);
-  }, [count]);
-
+    setCount(count + 1)
+  },[count])
   return (
     <div>
-      <h2>
-        {count}
-      </h2>
-      <button onClick={()=>setCount(count+1)}>+</button>
-      <button onClick={()=>setCount(count-1)}>-</button>
+      <p>{count}</p>
+      <button onClick={()=>setCount(count + 1)}>+</button>
     </div>
-  );
-};
+  )
+}
 
-
-export default First;
+export default First
