6240f40fde6d601c4f0b077efc33194870efe414
9mynamemj <ka5560@naver.com>
2023-05-15 20:48:35 +0900

    server, client 연결

--- FE/React/mj-app/src/routes/index.tsx
+++ FE/React/mj-app/src/routes/index.tsx
@@ -6,7 +6,9 @@ export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" />
         <Route path="/main" element={<Main />} />
+        <Route path="/login" element={<Main />} />
       </Routes>
       <Link to="/main"></Link>
       <Link to="/main">main</Link>
