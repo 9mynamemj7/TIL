56830d4e9fac1f4777384e36059ee345905490d7
9mynamemj <ka5560@naver.com>
2023-05-15 13:10:37 +0900

    Revert "시연용 페이지_로그인폼"
    This reverts commit 340373a7bccdc7a269041f2c63895ce4f5c67927.

--- FE/React/mj-app/client/src/pages/Main.tsx
+++ FE/React/mj-app/client/src/pages/Main.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import NowTime from '../components/NowTime'
-import { LogIn } from '../components/LogIn'
 
 export const Main = () => {
   return (
     <>
       <div><NowTime /></div>
-      <div><LogIn /></div>
     </>
   )
 }
