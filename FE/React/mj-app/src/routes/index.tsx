86ecd9e6df137a2188bb7af974b9960340a5475f
9mynamemj <ka5560@naver.com>
2023-05-16 16:41:49 +0900

    시연용 페이지_ignore전

--- FE/React/mj-app/src/routes/index.tsx
+++ FE/React/mj-app/src/routes/index.tsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Main } from '../pages/Main';
+import { Home } from '../components/Home';
+import { Login } from '../pages/Login';
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" />
-        <Route path="/main" element={<Main />} />
-        <Route path="/login" element={<Main />} />
-      </Routes>
-      <Link to="/main"></Link>
-      <Link to="/main">main</Link>
-    </BrowserRouter>
-  )
+    <Routes>
+      <Route index element={<Home/>} />
+      <Route path="/main" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
