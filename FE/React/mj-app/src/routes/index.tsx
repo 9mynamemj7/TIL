759cee8bb85f6056b7d954182a38421a4d63f8a2
9mynamemj <ka5560@naver.com>
2023-05-18 18:53:24 +0900

    시연용 페이지_CSS작업중

--- FE/React/mj-app/src/routes/index.tsx
+++ FE/React/mj-app/src/routes/index.tsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
-import { Main } from '../pages/Main';
-import { Home } from '../components/Home';
+import { Main } from '../pages/MainPage';
+import { Home } from '../pages/HomePage';
 import { Login } from '../pages/Login';
+import RegisterPage from '../pages/RegisterPage';
 
 export default function AppRoutes() {
   return (
@@ -9,6 +10,8 @@ export default function AppRoutes() {
       <Route index element={<Home/>} />
       <Route path="/main" element={<Main />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/success" element={<Main />} />
     </Routes>
   );
 }
